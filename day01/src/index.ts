554c024b0562b7d1d987d7a7b2cd07295769b30c
Svante Pihl <svante.pihl@cygni.se>
2022-11-30 18:39:25 +0100

    index file

--- day01/src/index.ts
+++ day01/src/index.ts
@@ -2,15 +2,15 @@ import * as fs from 'fs';
 
 const inputString = fs.readFileSync('input.txt', 'utf-8');
 
-const part = process.env.part || 'part1';
-
-if (part === 'part1') console.log(getSolutionPartOne(inputString));
-else console.log(getSolutionPartTwo(inputString));
-
 export function getSolutionPartOne(inputString: string): string {
-  throw new Error('Function not implemented.');
+  throw new Error('Not implemented');
 }
 
 export function getSolutionPartTwo(inputString: string): string {
   throw new Error('Function not implemented.');
 }
+
+const part = process.env.part || 'part1';
+
+if (part === 'part1') console.log(getSolutionPartOne(inputString));
+else console.log(getSolutionPartTwo(inputString));
