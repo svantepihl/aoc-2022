7f2ee54f7241aa02ecc9a849d6d55dfca821027d
Svante Pihl <svante.pihl@cygni.se>
2022-12-01 00:19:58 +0100

    formatting

--- day01/src/index.test.ts
+++ day01/src/index.test.ts
@@ -1,13 +1,13 @@
-import {getSolutionPartOne, getSolutionPartTwo} from '.';
+import { getSolutionPartOne, getSolutionPartTwo } from ".";
 
-describe('getSolutionPartOne', () => {
-  it('should throw', () => {
-    expect(() => getSolutionPartOne('')).toThrow();
+describe("getSolutionPartOne", () => {
+  it("should throw", () => {
+    expect(() => getSolutionPartOne("")).toThrow();
   });
 });
 
-describe('getSolutionPartTwo', () => {
-  it('should throw', () => {
-    expect(() => getSolutionPartTwo('')).toThrow();
+describe("getSolutionPartTwo", () => {
+  it("should throw", () => {
+    expect(() => getSolutionPartTwo("")).toThrow();
   });
 });
