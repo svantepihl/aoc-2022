d13d69026fda77196f4ad11e444c375f3115f1c2
Svante Pihl <svante.pihl@cygni.se>
2022-12-01 00:20:55 +0100

    Formatting

--- day01/src/index.ts
+++ day01/src/index.ts
@@ -1,16 +1,16 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-const inputString = fs.readFileSync('input.txt', 'utf-8');
+const inputString = fs.readFileSync("input.txt", "utf-8");
 
 export function getSolutionPartOne(inputString: string): string {
-  throw new Error('Not implemented');
+  throw new Error("Not implemented");
 }
 
 export function getSolutionPartTwo(inputString: string): string {
-  throw new Error('Function not implemented.');
+  throw new Error("Function not implemented.");
 }
 
-const part = process.env.part || 'part1';
+const part = process.env.part || "part1";
 
-if (part === 'part1') console.log(getSolutionPartOne(inputString));
+if (part === "part1") console.log(getSolutionPartOne(inputString));
 else console.log(getSolutionPartTwo(inputString));
