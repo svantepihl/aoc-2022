02b3e46bbeabd4fe1da3c09f5b0ebefb3c8088db
Svante Pihl <svante.pihl@cygni.se>
2022-12-01 20:56:28 +0100

    Day 2 ready

---
+++ day02/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine((Environment.GetEnvironmentVariable("part") == "part1")
+? PartOne(File.ReadAllText("input.txt"))
+: PartTwo(File.ReadAllText("input.txt")));
+int PartOne(string input) => 0; 
+int PartTwo(string input) => 0;
