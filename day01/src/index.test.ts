522972d3e275512cb3ff52db1b6e04c8081adbca
Svante Pihl <svante.pihl@cygni.se>
2022-12-01 06:35:35 +0100

    Day One!

--- day01/src/index.test.ts
+++ day01/src/index.test.ts
@@ -1,13 +1,28 @@
 import { getSolutionPartOne, getSolutionPartTwo } from ".";
 
+const exampleInput = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
 describe("getSolutionPartOne", () => {
-  it("should throw", () => {
-    expect(() => getSolutionPartOne("")).toThrow();
+  it("should return the correct solution", () => {
+    expect(getSolutionPartOne(exampleInput)).toBe("24000");
   });
 });
 
 describe("getSolutionPartTwo", () => {
-  it("should throw", () => {
-    expect(() => getSolutionPartTwo("")).toThrow();
+  it("should return the correct solution", () => {
+    expect(getSolutionPartTwo(exampleInput)).toBe("45000");
   });
 });
--- day01/src/index.ts
+++ day01/src/index.ts
@@ -2,12 +2,22 @@ import * as fs from "fs";
 
 const inputString = fs.readFileSync("input.txt", "utf-8");
 
-export function getSolutionPartOne(inputString: string): string {
-  throw new Error("Not implemented");
+const parseInput = (input: string): number[] => {
+  return input
+    .split("\n\n")
+    .map((group) => group.split("\n"))
+    .map((elf) => elf.reduce((acc, cur) => acc + parseInt(cur, 10), 0))
+    .sort((a, b) => b - a);
+};
+
+export function getSolutionPartOne(input: string): number {
+  return parseInput(input)[0];
 }
 
-export function getSolutionPartTwo(inputString: string): string {
-  throw new Error("Function not implemented.");
+export function getSolutionPartTwo(input: string): number {
+  return parseInput(input)
+    .splice(0, 3)
+    .reduce((acc, cur) => acc + cur, 0);
 }
 
 const part = process.env.part || "part1";
